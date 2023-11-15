a310123f5314d0e7f41dd930da97b56923cc95ad
eternum-dev <113004630+eternum-dev@users.noreply.github.com>
2023-11-15 16:29:47 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,4 +1,8 @@
 function skillMenbers(){
-    console.log('skillMenbers');
-        
+    this.name = 'skillMenbers';
+    this.skill = ['html', 'css', 'js', 'react', 'nodejs'];
+    this.show = function(){
+        console.log(this.name + ' : ' + this.skill);
+    }
+    
 }
